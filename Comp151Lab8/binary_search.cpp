a5bc00b416d3aae5d4d24bd2f23883a6e065a746
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2018-02-03 16:16:07 -0600

    Finished Binary Search

--- Comp151Lab8/binary_search.cpp
+++ Comp151Lab8/binary_search.cpp
@@ -6,7 +6,7 @@
 #include<iostream>
 using namespace std;
 
-int binarySearch(int [], int, int);  // function prototype
+int binarySearch(int[], int, int);  // function prototype
 
 const int SIZE = 16;
 
@@ -14,19 +14,19 @@ int main()
 {
 	int found, value;
 	//int array[] = {34,19,19,18,17,13,12,12,12,11,9,5,3,2,2,0}; // array to be searched
-	int array[] =   {0,2,2,3,5,9,11,12,12,12,13,17,18,19,19,34};
-	
+	int array[] = { 0,2,2,3,5,9,11,12,12,12,13,17,18,19,19,34 };
+
 	cout << "Enter an integer to search for:" << endl;
 	cin >> value;
 
 	found = binarySearch(array, SIZE, value); //function call to perform the binary search
-	                                          //on array looking for an occurrence of value
+											  //on array looking for an occurrence of value
 	if (found == -1)
 		cout << "The value " << value << " is not in the list" << endl;
 	else
 	{
 		cout << "The value " << value << " is in position number "
-		     << found + 1 << " of the list" << endl; 
+			<< found + 1 << " of the list" << endl;
 	}
 	return 0;
 }
@@ -43,27 +43,46 @@ int main()
 //                not found
 //
 //*******************************************************************
-int binarySearch(int array[],int numElems,int value) //function heading
+int binarySearch(int array[], int numElems, int value) //function heading
 {
 	int first = 0;				    // First element of list
 	int last = numElems - 1;	    // last element of the list
 	int middle;					   // variable containing the current 
-	                                // middle value of the list
-	
-	while (first <= last)
-	{
-		middle = first + (last - first) / 2; 
-		   
-	if (array[middle] == value)
-		return middle;		       // if value is in the middle, we are done
-	                                 
-	else if (array[middle]>value)
-		last = middle - 1;		   // toss out the second remaining half of
-								   // the array and search the first 
-	else
-		first = middle + 1;		   // toss out the first remaining half of
-								   // the array and search the second
+									// middle value of the list
+
+	if (array[first] < array[last]) { // ascending order
+		while (first <= last) {
+			middle = first + (last - first) / 2;
+
+			if (array[middle] == value)
+				return middle;		       // if value is in the middle, we are done
+
+			else if (array[middle] > value)
+				last = middle - 1;		   // toss out the second remaining half of
+										   // the array and search the first 
+			else
+				first = middle + 1;		   // toss out the first remaining half of
+										   // the array and search the second
+		}
 	}
-	
+	else { // descending order
+		while (first <= last) {
+			middle = first + (last - first) / 2;
+
+			if (array[middle] == value)
+				return middle;		       // if value is in the middle, we are done
+
+			else if (array[middle] < value)
+				last = middle - 1;		   // toss out the second remaining half of
+										   // the array and search the first 
+			else
+				first = middle + 1;		   // toss out the first remaining half of
+										   // the array and search the second
+		}
+	}
+
+
+
+
 	return -1;					   // indicates that value is not in the array
 }
